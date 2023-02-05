0be070977df6e8306837d7a9bc112b6124fbe6d8
IliasovDamir <ilyasovdh@gmail.com>
2023-02-05 15:45:46 +0300

    feat: add main page layout

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,12 +1,22 @@
+import './header.css';
+import { lang } from '../translate/translate';
+
+export let isEngl: Boolean = true;
 export class HeaderComponent {
   async getHtml(): Promise<string> {
-    return `<header class="header">
-              <nav class="nav">
-                  <a href="#main" class="nav__link">Main</a>
-                  <a href="#dashboard" class="nav__link">Dashboard</a>
-                  <a href="#signup" class="nav__link">Sign Up</a>
-                  <a href="#login" class="nav__link">Login</a>
-              </nav>
-      </header> `;
+    return `<header class="top-nav">
+    <div><a class='top-nav-svg' href="#main">${isEngl ? lang.en.main.benchmark : lang.ru.main.benchmark}</a></div>
+    <input id="menu-toggle" type="checkbox"/>
+    <label class='menu-button-container' for="menu-toggle">
+    <div class='menu-button'></div>
+  </label>
+    <ul class="menu">
+    <li><a href="#main" class="nav__link">${isEngl ? lang.en.main.main : lang.ru.main.main}</a></li>
+      <li><a href="#dashboard" class="nav__link">${isEngl ? lang.en.main.dashboard : lang.ru.main.dashboard}</a></li>
+      <li><a href="#signup" class="nav__link">${isEngl ? lang.en.main.sign : lang.ru.main.sign}</a></li>
+      <li><a href="#login" class="nav__link">${isEngl ? lang.en.main.login : lang.ru.main.login}</a></li>
+    </ul>
+  </header>
+      `;
   }
 }
