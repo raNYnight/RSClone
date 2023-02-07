d9cebe1b6f60ccfb6b4ae1bc5c622bb351a87e64
AlmondChips <94008966+AlmondChips@users.noreply.github.com>
2023-02-07 20:33:42 +0300

    fix: uncomment necessary code for navigation to main

--- src/app/routerPaths.ts
+++ src/app/routerPaths.ts
@@ -3,11 +3,11 @@ import { LoginComponent } from "../components/pages/login/login";
 import { MainComponent } from "../components/pages/main/main";
 import { SignupComponent } from "../components/pages/registation/signup";
 import { paths } from "interfaces/paths";
-// import { gamesInfo } from "../utils/games-info";
+import { gamesInfo } from "../utils/games-info";
 
 export const routerPaths: paths = {
   dashboard: async () => await new DashboardComponent().getHtml(),
   signup: async () => await new SignupComponent().getHtml(),
   login: async () => await new LoginComponent().getHtml(),
-  // main: async () => await new MainComponent(gamesInfo).getHtml(),
+  main: async () => await new MainComponent(gamesInfo).getHtml(),
 };
