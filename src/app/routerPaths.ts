0acfc87767829b03ceb3d9d9bc937a3352753935
IliasovDamir <ilyasovdh@gmail.com>
2023-02-10 14:32:29 +0300

    refactor: instead of 6 game components made 1 common GamePageComponent

--- src/app/routerPaths.ts
+++ src/app/routerPaths.ts
@@ -4,22 +4,15 @@ import { MainComponent } from '../components/pages/main/main';
 import { SignupComponent } from '../components/pages/registation/signup';
 import { paths } from 'interfaces/paths';
 import { gamesInfo } from '../utils/games-info';
-import { ReactionComponent } from '../components/reaction/reaction';
-import { SequenceComponent } from '../components/sequence/sequence';
-import { AimComponent } from '../components/aim/aim';
-import { NumberComponent } from '../components/number/number';
-import { VerbalComponent } from '../components/verbal/verbal';
-import { TypingComponent } from '../components/typing/typing';
+import { GamePageComponent } from '../components/gamePage/gamePage';
 
 export const routerPaths: paths = {
   dashboard: () => new DashboardComponent(),
   signup: () => new SignupComponent(),
   login: () => new LoginComponent(),
   main: () => new MainComponent(gamesInfo),
-  reaction: () => new ReactionComponent(),
-  sequence: () => new SequenceComponent(),
-  aim: () => new AimComponent(),
-  number: () => new NumberComponent(),
-  verbal: () => new VerbalComponent(),
-  typing: () => new TypingComponent(),
 };
+
+gamesInfo.forEach((game) => {
+  routerPaths[game.href] = () => new GamePageComponent(game.id);
+});
--- src/components/gamePage/gamePage.ts
+++ src/components/gamePage/gamePage.ts
@@ -1,4 +1,4 @@
-import { GameGreetingComponent } from '../game-area/game-area';
+import { GameAreaComponent } from '../game-area/game-area';
 import { gamesInfo } from '../../utils/games-info';
 import { GamesStatsComponent } from '../games-stats/games-stats';
 import { GameAboutComponent } from '../game-about/game-about';
@@ -7,21 +7,19 @@ import { Igame } from 'utils/types';
 
 export class GamePageComponent {
 
-  game: Igame;
+  id: number;
 
-  constructor(game: Igame) {
-    this.game = game;
+  constructor(id: number) {
+    this.id = id;
   }
 
   async getHtml(): Promise<string> {
 
-    const id: number = 2;
+    const areaSection = new GameAreaComponent(this.id);
+    const statsSection = new GamesStatsComponent(this.id);
+    const aboutSection = new GameAboutComponent(this.id);
 
-    const greetingSection = new GameGreetingComponent(ID_REACTION, gamesInfo);
-    const statsSection = new GamesStatsComponent(ID_REACTION, gamesInfo);
-    const aboutSection = new GameAboutComponent(ID_REACTION, gamesInfo);
-
-    return `${await greetingSection.getHtml()}
+    return `${await areaSection.getHtml()}
     <section class="game-info">      
       ${await statsSection.getHtml()}
       ${await aboutSection.getHtml()}
