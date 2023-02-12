dde3d08d1ed493e9301148162a16d40f2290236c
ranynigth <rany4ka@gmail.com>
2023-02-12 12:37:49 +0300

    fix: prettier

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -35,12 +35,11 @@ export interface User {
   permalink: string;
 }
 
-
-export interface UserData{
-  userId?: number,
-  userName: string,
-  regDate: Date,
-  permalink?: string,
+export interface UserData {
+  userId?: number;
+  userName: string;
+  regDate: Date;
+  permalink?: string;
 }
 
 export interface PlayedTest {
